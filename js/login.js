ae221fe3c85b9c075ae1436225e8cb69c009e182
dasjoy7 <dasjoy8709@gmail.com>
2024-10-16 11:24:30 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -2,13 +2,38 @@
 
 
 //step 1: set event handler
-document.getElementById('button-login').addEventListener('click', function(event){
+// document.getElementById('button-login').addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('login button clicked');
+
+
+
+//     //get the phone number
+//     const phonenumber = document.getElementById('phone-number').value;
+//     const pinnumber= document.getElementById('pin-number').value;
+//     console.log(phonenumber, pinnumber);
+
+//     if(phonenumber==='5'&&pinnumber==='1234')
+//     {
+//         console.log('you are logged in');
+//     }
+//     else{
+//         alert('Wrong phone number and pin');
+//     }
+// });
+
+document.getElementById('button-login').addEventListener('click', function (event) {
     event.preventDefault();
-    console.log('login button clicked');
 
-
-
-    //get the phone number
     const phonenumber = document.getElementById('phone-number').value;
-    console.log(phonenumber);
+    const pinnumber = document.getElementById('pin-number').value;
+    // console.log(phonenumber, pinnumber);
+    if(phonenumber==='5' && pinnumber === '1234')
+    {
+        console.log('you are logged in');
+        window.location.href='/home.html';
+    }
+    else{
+        alert('wrong phone number');
+    }
 });

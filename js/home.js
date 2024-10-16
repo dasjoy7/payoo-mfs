fbb87fafeedae9e090c927ab4deff5479fbcc2eb
dasjoy7 <dasjoy8709@gmail.com>
2024-10-16 13:09:12 +0600

    third

--- js/home.js
+++ js/home.js
@@ -5,9 +5,28 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     //prevent page reload after form submit
     event.preventDefault();
     const addMoneyInput = document.getElementById('input-add-money').value;
-    console.log(addMoneyInput);
+    // console.log(typeof addMoneyInput);
 
     //get the pin number provided
     const pinNumber = document.getElementById('input-pin-number').value;
-    console.log(pinNumber);
+    // console.log(typeof pinNumber);
+
+    //verify the pin number
+    if(pinNumber ==='1234')
+    {
+        console.log("adding money to your account");
+
+        //step 4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        // console.log(balance);
+
+        //step 5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber+balanceNumber;
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert("failed to add money");
+    }
 });
